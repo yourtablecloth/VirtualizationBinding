f29a358cba2e50996c2e791bc337bf0e9e69f2fb
Jung Hyun, Nam <jhn@mz.co.kr>
2024-02-16 18:28:55 +0900

    Add empty-body interfaces

--- ApiDefinition.cs
+++ ApiDefinition.cs
@@ -2,4 +2,128 @@ using Foundation;
 
 namespace VirtualizationBinding {
 
+    [BaseType(typeof(NSObject))]
+    interface VZAudioDeviceConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZAudioInputStreamSource {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZAudioOutputStreamSink {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZBootLoader : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZConsoleDevice {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZConsoleDeviceConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZDirectoryShare {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZDirectorySharingDevice {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZDirectorySharingDeviceConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZEntropyDeviceConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZGraphicsDeviceConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZGraphicsDisplayConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZLinuxRosettaCachingOptions {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZKeyboardConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZMemoryBalloonDevice {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZMemoryBalloonDeviceConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZNetworkDeviceAttachment {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZPlatformConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZPointingDeviceConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZSerialPortAttachment {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZSocketDevice {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZSocketDeviceConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZStorageDeviceAttachment {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZVirtioSoundDeviceStreamConfiguration : INSCopying {
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface VZVirtualMachineStartOptions {
+
+    }
 }
